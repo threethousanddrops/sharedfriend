4d45d0acedbd87bd511f5e611797d26451dac024
threethousanddrops <62058640+threethousanddrops@users.noreply.github.com>
2020-11-01 09:53:25 +0800

    Update CommonFriend.java

--- src/main/java/com/lyyq/CommonFriend.java
+++ src/main/java/com/lyyq/CommonFriend.java
@@ -1,134 +1,133 @@
 package com.lyyq;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.*; 
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.Configuration; 
+import org.apache.hadoop.fs.Path; 
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.io.Text; 
+import org.apache.hadoop.mapreduce.Job; 
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat; 
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat; 
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat; 
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat; 
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.fs.FileSystem; 
+
 
 public class CommonFriend {
-
-  public static Set<String> FindCommon(Set<String> list1, Set<String> list2){
-		if(list1==null || list2 == null){
-			return null;
-		}//no common friend
-		Set<String> result = new TreeSet<String>();
-		for (String frnd : list1) {
-			if(list2.contains(frnd)){
-				result.add(frnd);
+	public static class MyMapper1 extends Mapper <LongWritable,Text,Text,Text> {
+		
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line=value.toString();
+			String[] person_friends=line.split(", ");     //3.0
+			String person=person_friends[0];
+			String[] friends=person_friends[1].split(" ");     //3.0
+			for (String friend:friends){
+				context.write(new Text(friend),new Text(person));
 			}
 		}
-		return result;
+	}
+
+    public static class MyReduce1 extends Reducer <Text,Text,Text,Text> {
+		
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			StringBuffer bf= new StringBuffer();
+			for (Text friend:values){
+				bf.append(friend.toString()).append(",");
+			}
+			bf=bf.deleteCharAt(bf.length()-1);
+			context.write(key,new Text(String.valueOf(bf)));
+		}
 	}
 
 	
-	static class MyMapper extends Mapper<LongWritable, Text, Text, Text>{
-		private static Text outKey = new Text();
-		private static Text outValue = new Text();
+	public static class MyMapper2 extends Mapper <LongWritable,Text,Text,Text> {
 		
-		@Override
-		protected void map(LongWritable key, Text value, Context context)
-				throws IOException, InterruptedException {
-			String [] input = value.toString().split(",");
-			if(input.length != 2){
-				System.err.println("irregular input");
-				return;
-			}
-			outValue.set(input[1]);
-			String [] friend = input[1].split(" ");
-			for (String tofnd : friend) {
-				if(input[0].compareTo(tofnd) < 0){
-					outKey.set("([" + input[0] + ", " + tofnd + "],");
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			String[] friend_persons = line.split("\t");
+			String friend = friend_persons[0];
+			String[] persons = friend_persons[1].split(",");
+			Arrays.sort(persons); //排序
+			//两两配对
+			for (int i=0;i<persons.length-1;i++){
+				for (int j=i+1;j<persons.length;j++){
+					context.write(new Text("(["+persons[i]+","+persons[j]+"],"),new Text(friend));   //2.0 改输出格式
 				}
-				else {
-					outKey.set("([" + tofnd + ", " + input[0] + "],");
-				}
-				context.write(outKey, outValue);
-			}
-		}
-	}//[100,200] 200 300 400 500
-	//[100,300] 200 300 400 500
-	//[200,100] 300 400
-	
-	static class MyReducer extends Reducer<Text, Text, Text, Text>{
-
-		private Text outValue = new Text();
-		
-		@Override
-		protected void reduce(Text key, Iterable<Text> value, Context context)
-				throws IOException, InterruptedException {
-			Set<String> list1 = new TreeSet<String>();
-			Set<String> list2 = new TreeSet<String>();
-			ArrayList<String> friendlist = new ArrayList<String>();
-			int valnum = 0;
-			for (Text val : value) {
-				friendlist.add(val.toString());
-				valnum++;
-			}
-			if(valnum != 2){
-				return;//one is the friendlist of 0,another one is the friendlist of 1;
-			}
-			String [] frndlst1 = friendlist.get(0).trim().split(" ");
-			for (String s : frndlst1) {
-				list1.add(s);
-			}
-			String [] frndlst2 = friendlist.get(1).trim().split(" ");
-			for (String s : frndlst2) {
-				list2.add(s);
-			}
-			Set<String> frndlst = FindCommon(list1, list2);
-
-			StringBuilder shared = new StringBuilder();
-			shared.append("[");
-			for (String s : frndlst) {
-				shared.append(s + ", ");
-			}
-			String res = null;
-			if(shared.length() > 1){
-				res = shared.substring(0, shared.length()-2);//cut out "," & " "  
-			}
-			res+="])";
-		    this.outValue.set(res);
-			context.write(key, outValue);
+			}    
 		}
 	}
  
-	public static void main(String[] args) 
-			throws IOException, ClassNotFoundException, InterruptedException {
-	Configuration conf = new Configuration();
-	String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    if (otherArgs.length < 2) {
-      System.err.println("Usage: commmon <in> [<in>...] <out>");
-      System.exit(2);
-    }
-
-    Job job=Job.getInstance(conf);
-	job.setJarByClass(CommonFriend.class);
-	job.setMapperClass(MyMapper.class);
-	job.setReducerClass(MyReducer.class);
-	job.setMapOutputKeyClass(Text.class);
-	job.setMapOutputValueClass(Text.class);
-	job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(Text.class);
-    
-    FileSystem fs = FileSystem.get(conf);
-    Path outPath=new Path(otherArgs[otherArgs.length - 1]);
-    if (fs.exists(outPath)) {
-        fs.delete(outPath, true);
-    }
-	for (int i = 0; i < otherArgs.length - 1; ++i) {
-      FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
-    }
-    FileOutputFormat.setOutputPath(job,outPath);
-	System.exit(job.waitForCompletion(true) ? 0 : 1);
+	public static class MyReduce2 extends Reducer <Text,Text,Text,Text> {
+		
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			StringBuffer bf=new StringBuffer();
+			Set<String> set=new HashSet<>();
+			for (Text s:values){
+				if (!set.contains(s.toString())){
+					set.add(s.toString());
+				}
+			}
+			for (String s:set){
+				bf.append(s).append(",");
+			}
+			bf = bf.deleteCharAt(bf.length()-1);
+			context.write(key,new Text("["+bf.toString()+"])"));    //2.0
+		}
 	}
+
+		
+    public static void main(String[] args) throws Exception{
+		
+        Configuration conf=new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length < 2) {
+		  System.err.println("Usage: commmon <in> [<in>...] <out>");
+		  System.exit(2);
+		}
+		
+		Path TempPath = new Path("hdfs://lyyq181850099-master:9000/wordcount/FriendsTempOutput");
+		FileSystem fs = FileSystem.get(conf);
+		Path outPath=new Path(otherArgs[otherArgs.length - 1]);
+		if (fs.exists(outPath)) {
+			fs.delete(outPath, true);
+		}      
+		
+		Job job1=Job.getInstance(conf);
+        job1.setJarByClass(CommonFriend.class);
+        job1.setMapperClass(MyMapper1.class);
+        job1.setReducerClass(MyReduce1.class);
+        job1.setMapOutputKeyClass(Text.class);
+        job1.setMapOutputValueClass(Text.class);
+        job1.setOutputKeyClass(Text.class);
+        job1.setOutputValueClass(Text.class);
+        job1.setOutputFormatClass(TextOutputFormat.class);
+		job1.setInputFormatClass(TextInputFormat.class);
+		for (int i = 0; i < otherArgs.length - 1; ++i) {
+			FileInputFormat.addInputPath(job1, new Path(otherArgs[i]));
+		  }
+        FileOutputFormat.setOutputPath(job1,TempPath);
+		job1.waitForCompletion(true); 
+
+		Job job2=Job.getInstance(conf);
+        //Job job2=new Job(conf,"job2");
+		FileInputFormat.addInputPath(job2,TempPath);
+        job2.setMapperClass(MyMapper2.class);
+        job2.setReducerClass(MyReduce2.class);
+        job2.setMapOutputKeyClass(Text.class);
+        job2.setMapOutputValueClass(Text.class);
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(Text.class);
+        job2.setOutputFormatClass(TextOutputFormat.class);
+        job2.setInputFormatClass(TextInputFormat.class);
+        FileOutputFormat.setOutputPath(job2,outPath);
+		job2.waitForCompletion(true); 
+		
+		FileSystem.get(conf).delete(TempPath, true);  
+		System.exit(job1.waitForCompletion(true)?0:1);
+    }
 }
